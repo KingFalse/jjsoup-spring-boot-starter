37da21eb4c83fb2c162d34877716a757c4646efc
Veronica <yzsl@live.com>
2018-06-24 02:36:49 +0800

    update

--- src/main/java/me/kagura/HttpConnection.java
+++ src/main/java/me/kagura/HttpConnection.java
@@ -42,7 +42,6 @@ public abstract class HttpConnection implements ConnectionX {
      * @return
      */
     public HttpConnection connect(@NonNull String url) {
-        System.err.println(initHttpConnection == null);
         try {
             HttpConnection httpConnection = (HttpConnection) beanConfig.getHttpConnection()
                     .url(url)
