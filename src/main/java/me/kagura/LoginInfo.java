542838853d4230c869802b24c0d7bdc182034b98
ZFang <FangZhuo@WeCash.Net>
2018-08-10 18:23:50 +0800

    update

--- src/main/java/me/kagura/LoginInfo.java
+++ src/main/java/me/kagura/LoginInfo.java
@@ -3,6 +3,7 @@ package me.kagura;
 import java.io.Serializable;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
+import java.net.SocketAddress;
 import java.util.*;
 
 /**
@@ -84,5 +85,22 @@ public class LoginInfo implements Serializable {
                 ", cookies=" + cookies +
                 ", proxy=" + proxy +
                 '}';
+    }
+}
+
+class KProxy implements Serializable {
+    public Proxy.Type type;
+    public SocketAddress sa;
+
+    public KProxy() {
+        type = Proxy.Type.DIRECT;
+        sa = null;
+    }
+
+    public KProxy(Proxy.Type type, SocketAddress sa) {
+        if ((type == Proxy.Type.DIRECT) || !(sa instanceof InetSocketAddress))
+            throw new IllegalArgumentException("type " + type + " is not compatible with address " + sa);
+        this.type = type;
+        this.sa = sa;
     }
 }
