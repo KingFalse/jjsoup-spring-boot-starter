877749192eeafa3263c19a4de81565d3f32aa6bb
ZFang <FangZhuo@WeCash.Net>
2018-08-13 19:13:15 +0800

    完善@LoginInfoKey注解作用在类上

--- src/main/java/me/kagura/AspectControllerLoginInfo.java
+++ src/main/java/me/kagura/AspectControllerLoginInfo.java
@@ -6,6 +6,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -19,25 +20,31 @@ public class AspectControllerLoginInfo {
     @Autowired(required = false)
     LoginInfoSerializable loginInfoSerializable;
 
-    @Around(value = "@within(org.springframework.stereotype.Controller) || @within(org.springframework.web.bind.annotation.RestController) && args(*,*,..)", argNames = "joinPoint")
+    @Around(value = "@within(me.kagura.anno.LoginInfoKey) || @within(org.springframework.stereotype.Controller) || @within(org.springframework.web.bind.annotation.RestController) && args(*,*,..)", argNames = "joinPoint")
     public Object Around(ProceedingJoinPoint joinPoint) throws Throwable {
         if (loginInfoSerializable == null) {
             return joinPoint.proceed();
         }
+
         Object[] args = joinPoint.getArgs();
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         Parameter[] parameters = signature.getMethod().getParameters();
+        String[] parameterNames = signature.getParameterNames();
+
+        LoginInfoKey annoLoginInfoKey = AnnotationUtils.findAnnotation(signature.getMethod().getDeclaringClass(), LoginInfoKey.class);
 
         int loginInfoKeyIndex = -1;
         int loginInfoIndex = -1;
         for (int i = 0; i < parameters.length; i++) {
             Parameter parameter = parameters[i];
-            LoginInfoKey annotation = parameter.getAnnotation(LoginInfoKey.class);
-            if (annotation != null) {
+            if (parameter.getAnnotation(LoginInfoKey.class) != null) {
                 loginInfoKeyIndex = i;
             }
             if (parameter.getType().equals(LoginInfo.class)) {
                 loginInfoIndex = i;
+            }
+            if (annoLoginInfoKey != null && parameterNames[i].equals(annoLoginInfoKey.value())) {
+                loginInfoKeyIndex = i;
             }
         }
         if (loginInfoIndex > -1 && loginInfoKeyIndex > -1) {
