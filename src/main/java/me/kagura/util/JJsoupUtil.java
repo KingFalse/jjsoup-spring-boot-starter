2c166bb211ea0bf98d22803c5fcf63342d811e40
ZFang <FangZhuo@WeCash.Net>
2018-10-19 22:22:47 +0800

    添加JJsoupUtil

---
+++ src/main/java/me/kagura/util/JJsoupUtil.java
@@ -0,0 +1,26 @@
+package me.kagura.util;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class JJsoupUtil {
+    /**
+     * 将document中所有相对路径的href跟src转换成绝对路径
+     *
+     * @param document
+     * @return
+     */
+    public static Document convertToAbsUrlDocument(Document document) {
+        Elements relativePathElements = document.select("[src^=./],[src^=../],[src^=/]:not([src^=//]),[src^=/]:not([src^=//]),[href^=./],[href^=../],[href^=/]:not([href^=//]),[href^=/]:not([href^=//])");
+        for (Element element : relativePathElements) {
+            if (element.hasAttr("href")) {
+                element.attr("href", element.attr("abs:href"));
+            }
+            if (element.hasAttr("src")) {
+                element.attr("src", element.attr("abs:src"));
+            }
+        }
+        return document;
+    }
+}
