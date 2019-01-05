4a43c3f406c774dd9cda9b01e5717c01714a5ffd
鹞之神乐 <yzsl@live.com>
2019-01-05 14:16:38 +0800

    增加JJsoupUtil.sleep(int ms);

--- src/main/java/me/kagura/util/JJsoupUtil.java
+++ src/main/java/me/kagura/util/JJsoupUtil.java
@@ -5,7 +5,15 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class JJsoupUtil {
+
+    static String script = System.getProperty("java.io.tmpdir") + "sleep.vbs";
+    static boolean isWin = System.getProperty("os.name").toLowerCase().startsWith("windows");
+
     /**
      * 将document中所有相对路径的href跟src转换成绝对路径，兼容伪协议
      *
@@ -32,6 +40,52 @@ public class JJsoupUtil {
 
         }
         return document;
+    }
+
+    /**
+     * 执行系统对应的脚本进行sleep操作，单位毫秒
+     *
+     * @param ms
+     */
+    public static void sleep(int ms) {
+        try {
+            if (isWin) {
+                sleepWin(ms);
+            } else {
+                sleepLinux(ms);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 调用Linux命令sleep，单位毫秒
+     *
+     * @param ms
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    private static void sleepLinux(int ms) throws IOException, InterruptedException {
+        Runtime.getRuntime().exec("sleep " + ms * 0.001).waitFor();
+    }
+
+    /**
+     * 调用vbs脚本sleep，单位毫秒
+     *
+     * @param ms
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    private static void sleepWin(int ms) throws IOException, InterruptedException {
+        if (!new File(script).exists()) {
+            FileWriter fileWriter = new FileWriter(script);
+            fileWriter.write("WScript.sleep WScript.Arguments(0)");
+            fileWriter.flush();
+            fileWriter.close();
+        }
+        String cmd = String.format("CScript.exe %s %d >nul", script, ms);
+        Runtime.getRuntime().exec(cmd).waitFor();
     }
 
 }
